7c16ff3cabd2461e5cb307c0ed845dbf71964c09
Vladimir Novikov <novikovvv-tver@yandex.ru>
2018-05-14 21:44:31 +0300

    Добавляет возможность сортировки фотографий объявления

--- js/photo.js
+++ js/photo.js
@@ -5,9 +5,13 @@
   var avatarChooser = document.querySelector('#avatar');
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
   var roomPhotoChooser = document.querySelector('#images');
-  var roomPhotoContainer = document.querySelector('.ad-form__photo-container');
+  var roomPhotoContainer = document.querySelector('.ad-form__photo-list');
+  var photoList = document.querySelector('.ad-form__photo-list');
   var defaultAvatar = 'img/muffin-grey.svg';
   var firstFileIndex = 0;
+  var draggedNode = null;
+  var replaceNode = null;
+  var draggedNodeSibling = null;
 
   var onInputTypeFileChange = function (evt) {
     var file = evt.target.files[firstFileIndex];
@@ -22,7 +26,7 @@
 
       reader.addEventListener('load', function () {
         if (evt.target === roomPhotoChooser) {
-          roomPhotoContainer.appendChild(window.photo.add(reader));
+          photoList.appendChild(window.photo.add(reader));
           return;
         }
         avatarPreview.src = reader.result;
@@ -32,20 +36,63 @@
     reader.readAsDataURL(file);
   };
 
+  var onPhotoListDragOver = function (evtOver) {
+    evtOver.preventDefault();
+    return false;
+  };
+
   avatarChooser.addEventListener('change', onInputTypeFileChange);
   roomPhotoChooser.addEventListener('change', onInputTypeFileChange);
+  photoList.addEventListener('dragover', onPhotoListDragOver);
 
   window.photo = {
     add: function (reader) {
-      var photoContainer = document.createElement('div');
+      var photoContainer = document.createElement('li');
       var photo = document.createElement('img');
+
+      var onRoomPhotoDragStart = function (evtStart) {
+        if (evtStart.target.tagName.toLowerCase() === 'img') {
+          draggedNode = evtStart.target.parentNode;
+          draggedNodeSibling = draggedNode.nextSibling;
+
+          evtStart.dataTransfer.effectAllowed = 'move';
+          evtStart.dataTransfer.setData('text/plain', draggedNode);
+        }
+      };
+
+      var onRoomPhotoDragEnter = function (evtEnter) {
+        if (evtEnter.target !== draggedNode) {
+          replaceNode = evtEnter.target.parentNode;
+        }
+      };
+
+      var onRoomPhotoDragEnd = function () {
+        if (replaceNode !== null) {
+          if (draggedNodeSibling === replaceNode) {
+            photoList.insertBefore(draggedNode, replaceNode.nextSibling);
+            draggedNode = null;
+            replaceNode = null;
+            return;
+          }
+
+          var swap = photoList.replaceChild(draggedNode, replaceNode);
+          photoList.insertBefore(swap, draggedNodeSibling);
+          draggedNode = null;
+          replaceNode = null;
+        }
+      };
 
       photo.src = reader.result;
       photo.width = 70;
       photo.height = 70;
 
       photoContainer.classList.add('ad-form__photo');
+      photoContainer.draggable = true;
       photoContainer.appendChild(photo);
+
+      photoContainer.addEventListener('dragstart', onRoomPhotoDragStart);
+      photoContainer.addEventListener('dragenter', onRoomPhotoDragEnter);
+      photoContainer.addEventListener('dragend', onRoomPhotoDragEnd);
 
       return photoContainer;
     },
